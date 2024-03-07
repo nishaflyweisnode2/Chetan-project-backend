3fa8caff2b6d164cb6663ddf9769aa9ac1bdd43a
rubiyadav <rubiflyeis@gmail.com>
2024-03-07 11:53:53 +0530

    07 march 2024 11:53am

--- Controller/addressCtrl.js
+++ Controller/addressCtrl.js
@@ -4,15 +4,13 @@ const Address = require("../Model/addressModel");
 const User = require("../Model/userModel");
 
 exports.createAddress = catchAsyncErrors(async (req, res, next) => {
-  console.log(req.user._id)
-  req.body.user = req.user._id;
-  const users = await User.findById({ _id: req.user._id });
+  const users = await User.findById({ _id: req.params.userId });
   if (!users) {
-    return next(new ErrorHander(`User does not exist with Id: ${req.params.id}`, 400));
+    return next(new ErrorHander(`User does not exist with Id: ${req.params.userId}`, 400));
   }
-  const findAddress = await Address.findOne({ user: req.user._id });
+  const findAddress = await Address.findOne({ user: users._id });
   if (findAddress) {
-    await Address.findByIdAndUpdate({ _id: findAddress._id }, { $set: req.body }, { new: true, runValidators: true, useFindAndModify: "pending", });
+    await Address.findByIdAndUpdate({ _id: findAddress._id }, { $set: req.body }, { new: true, });
     await User.findByIdAndUpdate({ _id: users._id }, { $set: { addressStatus: "pending", addressId: findAddress._id } }, { new: true, });
   }
   const address = await Address.create(req.body);
@@ -25,7 +23,7 @@ exports.getAddressById = catchAsyncErrors(async (req, res, next) => {
 });
 exports.updateAddress = catchAsyncErrors(async (req, res, next) => {
   const newAddressData = req.body;
-  await Address.findByIdAndUpdate(req.params.id, newAddressData, { new: true, runValidators: true, useFindAndModify: "pending", });
+  await Address.findByIdAndUpdate(req.params.id, newAddressData, { new: true });
   return res.status(200).json({ success: true, });
 });
 exports.deleteAddress = catchAsyncErrors(async (req, res, next) => {
--- Routes/addressRoute.js
+++ Routes/addressRoute.js
@@ -1,13 +1,8 @@
 const router = require("express").Router();
 const { isAuthenticatedUser } = require("../Middleware/auth");
-const {
-    createAddress,
-    getAddressById,
-    updateAddress,
-    deleteAddress,
-} = require("../Controller/addressCtrl");
+const { createAddress, getAddressById, updateAddress, deleteAddress, } = require("../Controller/addressCtrl");
 
-router.post("/api/v1/address/", isAuthenticatedUser, createAddress);
+router.post("/api/v1/address/:userId", createAddress);
 router.get("/api/v1/address/", isAuthenticatedUser, getAddressById);
 router.put("/api/v1/address/:id", updateAddress);
 router.delete("/api/v1/address/:id", deleteAddress);
